75ee6df2d46d60e5dbcdbfcd53125982289008fe
xyco <ignaciomoralet@gmail.com>
2024-06-21 13:29:33 -0400

    se intento mejorar la interaccion entre las clases, ahora al crear la comunidad con n ciudadanos, los n ciudadanos se agregan a un atributo de la clase comunidad (fijarse en que los ciudadanos se crean y se insertan en la clase comunidad y al momento de crearse hacen referencia a la misma clase comunidad), ademas se agrego la asiganacion de familias, descubri el random.gauss que es como el random pero con distribucion Normal, cosa que haria mas realista la distribucion de familias, en este codigo el numero de integrantes en cada hogar tiene de media 3.1 y desviacion estandar 1.2, datos que tienen que ser corroborados y consultados para estar mas seguro

--- comunidad.py
+++ comunidad.py
@@ -1,29 +1,63 @@
 from ciudadano import Ciudadano
+import random
 
 class Comunidad:
     def __init__(self, num_ciudadanos,
                         promedio_conexion_fisica,
                         enfermedad,
                         num_infectados,
-                        probabilidad_conexion_fisica):
+                        probabilidad_conexion_fisica,
+                        nombre):
 
         self.num_ciudadanos = num_ciudadanos
         self.promedio_conexion_fisica = promedio_conexion_fisica
         self.enfermedad = enfermedad
         self.num_infectados = num_infectados
         self.probabilidad_conexion_fisica = probabilidad_conexion_fisica
+        self.nombre = nombre
 
         #se crean ciudadanos base, sin comunidad, sin nombre, sin familia (el hombre sin identidad)
         #estos atributos de las personas se incorporaran en otra funcion
         self.ciudadanos = self.crear_ciudadanos(self.num_ciudadanos)
 
+    def numero_integrantes(self, media, desviacion_estandar):
+        #la cantidad de integrantes de la familia distribuJen normal con ciertos parametros qe hay q consultar mejor
+        #random .gauss retorna un valor random pero en dustribucion normal
+        #round redondea 3.7 -> 4 // 3.2 -> 3
+        return round(random.gauss(media, desviacion_estandar))
+
+
     def crear_ciudadanos(self, num_ciudadanos):
         #arreglo que alergara los obajeto tipo ciudadano
         arreglo_comunidad = []
+        """
+        id_familia = 0
+
+        media = 3.1
+        desviacion_estandar = 1.2
+
+        for i in range(num_personas):
+            cantidad_integrantes = numero_integrantes(media, desviacion_estandar)
+
+            for persona in range(cantidad_integrantes):
+                persona = Persona(id_persona=i+1, id_familia=id_familia)
+                personas.append(persona)
+
+            id_familia += 1 
+        return personas"""
+
+        id_familia = 0
+        media = 3.1
+        desviacion_estandar = 1.2
 
         #self.ciudadanos.append(ciudadano)
         for x in range(num_ciudadanos):
-            print(f"{x} agregar ciudadano")
-            x = Ciudadano("comunidad", x, "nombre_apellido", "familia")
-            arreglo_comunidad.append(x)
+            print(f"{x + 1} agregar ciudadano")
+
+            cantidad_integrantes = self.numero_integrantes(media, desviacion_estandar)
+            for persona in range(cantidad_integrantes):
+                #se crea la persona /haciendo que la clase se haga referencia hacia si misma (mind blowing cuando cache esto looooooool)
+                persona = Ciudadano(self, x + 1, f"nombre_apellido{x + 1}", id_familia)
+                arreglo_comunidad.append(persona)
+            id_familia += 1 
         return arreglo_comunidad
--- main.py
+++ main.py
@@ -4,20 +4,29 @@ from simulador import Simulador
 
 covid = Enfermedad(infeccion_probable=0.3,
                     promedio_pasos=10)
-talca = Comunidad(num_ciudadanos=17,
+talca = Comunidad(num_ciudadanos=200,
                     promedio_conexion_fisica=8,
                     enfermedad = covid,
                     num_infectados=10,
-                    probabilidad_conexion_fisica=0.8)
+                    probabilidad_conexion_fisica=0.8,
+                    nombre = "Talca")
 sim = Simulador()
 sim.set_comunidad(talca)
 sim.run(45)
 
 #print(sim.get_comunidad().ciudadanos)
 
+#devuelve un arreglo que contiene a todos los ciudadanos
 arreglo_ciudadanos = sim.get_comunidad().ciudadanos
 
 #consultar clase persona, ahora mismo los objetos ciudadano estan contenidos en un array que es
 #un atributo de comunidad (pensando en relacionar las dos clases de otra menera mmmmmmmmmmmmm)
 for persona in arreglo_ciudadanos:
-    print(persona._id)
+    print("comunidad ~ ", persona.comunidad.nombre)
+    print("id        ~ ", persona._id)
+    print("nombre    ~ ", persona.nombre_apellido)
+    print("familia   ~ ", persona.familia)
+    print("")
+
+#numpy      -> resolver ecuaciones diferenciales
+#matplotlib -> generar grafico
