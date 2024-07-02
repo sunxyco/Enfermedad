4be6c6d72653451460b0a5ea190aed56564d223e
xyco <ignaciomoralet@gmail.com>
2024-07-01 22:27:40 -0400

    se movió el archivo con apuntes a la carpeta Things, Se adjunto a la misma carpeta  un ejemplo de la propagacion de la enfermedad con el codigo actual - lo que hace ahora es que la enfermedad se contagia por todos los conocidos de la persona, mas adelante se hara con cierta probabilidad que se denotara en el main

--- simulador.py
+++ simulador.py
@@ -19,7 +19,7 @@ class Simulador:
         print("SIMULADOR ~~~~ ", self.get_comunidad().nombre)
 
         for iteracion in range(pasos):
-            print("PASO ~~ ", iteracion + 1)
+            print(f"PASO ~~ {iteracion + 1} \n")
 
             for persona in self.get_comunidad().ciudadanos:
                 print(f"id_{persona._id} {persona.estado} {persona.nombre_apellido} {persona.familia} {conexiones.mostrar_mostrar_coneciones(self, persona.conexiones)}")
@@ -35,21 +35,47 @@ class Simulador:
 
             #ciclo para enfermar a la gente
             #se recorre a todos los ciudadanos hasta que se encuentre a alguien que tiene la enfermedad
+            arreglo_personas_contagianes = []
             for test in self.get_comunidad().ciudadanos:
                 #si tiene la enfermada empieza el proceso aleatorio de enfermar a la demas gente
                 if test.estado:
+                    #arreglo de personas que pueden contagiar al resto
+                    arreglo_personas_contagianes.append(test)
                     print("~~~~~~~~~~~~~~~~funcion de contagiar xd")
-                    self.contagiar_conexiones(test)
+
+            #una ves extraidos las pesronas que tienen la enfermedad es hace la funcnion para que contagien
+            #de momento todas las conexiones que tiene la persona se va conttagiar
+            for persona_posible_de_contagio in arreglo_personas_contagianes:
+                self.contagiar_conexiones(persona_posible_de_contagio)
+
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nSe Terminó el dia \n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        print("\n\nDATOS FINALES DE LA SIMULACION\n")
+        for persona in self.get_comunidad().ciudadanos:
+            print(f"id_{persona._id} {persona.estado} {persona.nombre_apellido} {persona.familia} {conexiones.mostrar_mostrar_coneciones(self, persona.conexiones)}")
+
 
     def contagiar_conexiones(self, persona_contagiante):
         #se va a comparar persona cada una de las personas que tengan conexiones con la persona contagiante
         #si tienen el mismo _id_familiar la probabilidad de contagiarlos sera mayor, si es diferente sera menor
 
         #mejorar la funcion persona.contagiarse#self.enfermarse
-        print("persona contagiante ~~~ ", persona_contagiante.nombre_apellido)
+        print("\nDatos ~ persona contagiante ~~~ ", persona_contagiante.nombre_apellido)
 
         print("posibles contagios  ~~~ ", conexiones.mostrar_mostrar_coneciones(self, persona_contagiante.conexiones))
 
+        conexiones_temporal = persona_contagiante.conexiones
         #ciclo para contagiar a los demas
-        for persona_suseptible in persona_contagiante.conexiones:
+        
+        for persona_suseptible in conexiones_temporal:
             print(f"la persona es un contacto {persona_suseptible._id} ~ {persona_suseptible.nombre_apellido}")
+            #si la persona ya esta enferma ya no se tomara en cuenta
+            if persona_suseptible.estado:
+                print(f"Ya esta enfermo ~~ {persona_suseptible.nombre_apellido}")
+            #si la persona no esta enferma esta se mandara a la funcion para enfermarse
+            else:
+                persona_suseptible.enfermarse(persona_contagiante.enfermedad)
+                print(f"{persona_suseptible.nombre_apellido} ~~~ se ha enfermado")
+                print(f"~ {persona_suseptible.estado} \n")
+
+        #persona_contagiante.conexiones = conexiones_temporal
